481d471c99687d8040a6c939f239c1254abc9c36
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2024-08-13 06:50:00 +0200

    create basic fizzbuzz component

---
+++ src/js-algorithms/FizzBuzz.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent, useState } from 'react';
+
+// export interface Props { };
+
+const FizzBuzz: FunctionComponent = () => {
+
+    const [fizzBuzzResult, setFizzBuzzResult] = useState<Array<string>>([])
+
+    function fizzbuzz(num: number) {
+        const result: string[] = [];
+        for (let index = 1; index <= num; index++) {
+            if ((index % 3 === 0 && index % 5 === 0)) result.push('FizzBuzz')
+            else if (index % 3 === 0) result.push('Fizz')
+            else if (index % 5 === 0) result.push('Buzz')
+            else result.push(String(index))
+        }
+        setFizzBuzzResult(result)
+    }
+    const handleOnChangeInput = (event: React.BaseSyntheticEvent) => {
+        fizzbuzz(Number(event.target.value))
+    }
+
+    return (
+        <div>
+            <input type="string" onChange={handleOnChangeInput} placeholder='please pass in a number' />
+
+            {fizzBuzzResult.map(item => <div>{item}</div>)}
+
+        </div>
+    );
+};
+
+
+export default FizzBuzz;
